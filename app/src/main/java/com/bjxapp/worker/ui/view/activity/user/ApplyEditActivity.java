7a6db129d5eb4bddaec91e8333a0e937ef3706bb
zhangdan <zhangdan@cmcm.com>
2018-10-23 23:46:28 +0800

    add apply edit activity .

---
+++ app/src/main/java/com/bjxapp/worker/ui/view/activity/user/ApplyEditActivity.java
@@ -0,0 +1,26 @@
+package com.bjxapp.worker.ui.view.activity.user;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by zhangdan on 2018/10/23.
+ * <p>
+ * comments:
+ */
+
+public class ApplyEditActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+
+
+
+
+}
