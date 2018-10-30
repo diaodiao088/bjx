28dac87c4b1c2b084108ebb763f50703f90e39f4
zhangdan <zhangdan@cmcm.com>
2018-10-30 21:17:50 +0800

    refine UI performance .

--- app/src/main/java/com/bjxapp/worker/adapter/WithdrawAdapter.java
+++ app/src/main/java/com/bjxapp/worker/adapter/WithdrawAdapter.java
@@ -10,21 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.bjxapp.worker.controls.XTextView;
 import com.bjxapp.worker.model.WithdrawInfo;
 import com.bjxapp.worker.R;
 
-public class WithdrawAdapter extends BaseAdapter{
+public class WithdrawAdapter extends BaseAdapter {
 
     private LayoutInflater mInflater;
     private ArrayList<WithdrawInfo> aInfo;
-	
-	public WithdrawAdapter(Context context,ArrayList<WithdrawInfo> info) 
-	{
-		mInflater = LayoutInflater.from(context);
-		aInfo=info;
-	}
+
+    public WithdrawAdapter(Context context, ArrayList<WithdrawInfo> info) {
+        mInflater = LayoutInflater.from(context);
+        aInfo = info;
+    }
 
     public int getCount() {
         return aInfo.size();
@@ -37,65 +37,60 @@ public class WithdrawAdapter extends BaseAdapter{
     public long getItemId(int position) {
         return position;
     }
-	
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
 
-		ViewHolder holder;
-		
-		if (convertView == null) {
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+
+        if (convertView == null) {
             convertView = mInflater.inflate(R.layout.activity_balance_withdraw_item, null);
 
             holder = new ViewHolder();
-            holder.textViewStatus = (XTextView) convertView.findViewById(R.id.balance_withdraw_item_status);
-            holder.textViewDate = (XTextView) convertView.findViewById(R.id.balance_withdraw_item_date);
-            holder.textViewMoney = (XTextView) convertView.findViewById(R.id.balance_withdraw_item_money_edit);
-            
+            holder.textViewStatus = (TextView) convertView.findViewById(R.id.withdraw_history_content);
+            holder.textViewMoney = (TextView) convertView.findViewById(R.id.withdraw_history_money);
+
             convertView.setTag(holder);
-        } 
-        else 
-        {
+        } else {
             holder = (ViewHolder) convertView.getTag();
         }
-		
+
         String statusString = "";
         switch (aInfo.get(position).getStatus()) {
-		case 0:
-			statusString = "待处理";
-			break;
-		case 1:
-			statusString = "提现成功";
-			break;
-		case 2:
-			statusString = "提现失败";
-			break;	
-		default:
-			break;
-		}
-        
-        holder.textViewDate.setText(getFormatDateString(aInfo.get(position).getDate()));
-        holder.textViewMoney.setText(aInfo.get(position).getMoney() + "元");
-        holder.textViewStatus.setText(statusString);
-        
-		return convertView;
-	}
-	
-	public String getFormatDateString(String dateString) {
-		DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-		DateFormat toFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
-		Date date = null;
-		try {
-			date = format.parse(dateString);
-			return toFormat.format(date);
-		} 
-		catch (Exception e) {
-			return dateString;
-		}
-	}
+            case 0:
+                statusString = "待处理";
+                break;
+            case 1:
+                statusString = "提现成功";
+                break;
+            case 2:
+                statusString = "提现失败";
+                break;
+            default:
+                break;
+        }
+
+        //  holder.textViewDate.setText(getFormatDateString(aInfo.get(position).getDate()));
+        //  holder.textViewMoney.setText(aInfo.get(position).getMoney() + "元");
+        //  holder.textViewStatus.setText(statusString);
+
+        return convertView;
+    }
+
+    public String getFormatDateString(String dateString) {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        DateFormat toFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
+        Date date = null;
+        try {
+            date = format.parse(dateString);
+            return toFormat.format(date);
+        } catch (Exception e) {
+            return dateString;
+        }
+    }
 
     class ViewHolder {
-    	XTextView textViewStatus;
-    	XTextView textViewDate;
-    	XTextView textViewMoney;
+        TextView textViewStatus;
+        TextView textViewMoney;
     }
 }
