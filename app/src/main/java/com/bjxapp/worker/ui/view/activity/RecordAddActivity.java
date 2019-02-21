21ea5db3f5472057ad1095d4645346213136ee46
zhangdan <zhangdan@cmcm.com>
2019-02-21 21:46:53 +0800

    add image picker .

--- app/src/main/java/com/bjxapp/worker/ui/view/activity/RecordAddActivity.java
+++ app/src/main/java/com/bjxapp/worker/ui/view/activity/RecordAddActivity.java
@@ -1,18 +1,45 @@
 package com.bjxapp.worker.ui.view.activity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bjx.master.R;
 import com.bjxapp.worker.controls.XTextView;
+import com.bjxapp.worker.ui.view.activity.widget.SpaceItemDecoration;
 import com.bjxapp.worker.ui.widget.DimenUtils;
+import com.bjxapp.worker.ui.widget.RoundImageView;
 import com.bjxapp.worker.utils.DateUtils;
+import com.bjxapp.worker.utils.SDCardUtils;
+import com.bjxapp.worker.utils.UploadFile;
+import com.bjxapp.worker.utils.Utils;
+import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +47,8 @@ import butterknife.OnClick;
 import cn.qqtheme.framework.picker.DatePicker;
 import cn.qqtheme.framework.picker.OptionPicker;
 import cn.qqtheme.framework.widget.WheelView;
+
+import static com.bjxapp.worker.global.Constant.REQUEST_CODE_CLOCK_TAKE_PHOTO;
 
 public class RecordAddActivity extends Activity {
 
@@ -40,13 +69,22 @@ public class RecordAddActivity extends Activity {
     @BindView(R.id.record_status_tv)
     TextView mRecordStatusTv;
 
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    private GridLayoutManager mGridLayoutManager;
+    private MyAdapter mAdapter;
+
+    private static final int MAX_PIC_COUNT = 20;
+    private static final int FEEDBACK_LOAD_IMAGES_RESULT = 1;
+
     @OnClick(R.id.record_status_tv)
     void onClickStatus() {
         showStatusPicker();
     }
 
     @OnClick(R.id.record_time_tv)
-    void onClickTime(){
+    void onClickTime() {
         showTimerPicker();
     }
 
@@ -59,12 +97,31 @@ public class RecordAddActivity extends Activity {
     @BindView(R.id.title_text_tv)
     XTextView mTitleTextView;
 
+    private ArrayList<ImageBean> mImageList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.record_add_activity);
         ButterKnife.bind(this);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mGridLayoutManager = new GridLayoutManager(this, 4);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mRecyclerView.addItemDecoration(new SpaceItemDecoration(4, 50, true));
+        mAdapter = new MyAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void initData() {
+        ImageBean bean = new ImageBean(ImageBean.TYPE_IMAGE, "");
+        mImageList.add(bean);
+        mAdapter.setList(mImageList);
+        mAdapter.notifyDataSetChanged();
     }
 
     public static void gotoActivity(Activity context, int requestCode) {
@@ -140,6 +197,306 @@ public class RecordAddActivity extends Activity {
         });
         picker.show();
     }
+
+    private class MyAdapter extends RecyclerView.Adapter {
+
+        private ArrayList<ImageBean> mList;
+
+        public MyAdapter() {
+
+        }
+
+        public void setList(ArrayList<ImageBean> imageBean) {
+            this.mList = imageBean;
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            RecyclerView.ViewHolder mHolder = null;
+
+            if (viewType == ImageBean.TYPE_ADD) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_image_item, parent, false);
+                mHolder = new VH_IMAGE_ITEM(view);
+            } else if (viewType == ImageBean.TYPE_IMAGE) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_image_item_cl, parent, false);
+                mHolder = new VH_DELETE_ITEM(view);
+            }
+
+            return mHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+
+            final ImageBean bean = mList.get(position);
+
+            if (holder instanceof VH_IMAGE_ITEM) {
+
+                Glide.with(RecordAddActivity.this).load(bean.getUrl()).into(((VH_IMAGE_ITEM) holder).mIv);
+
+                ((VH_IMAGE_ITEM) holder).mDeleteIv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mListener != null) {
+                            mListener.deleteImage(position);
+                        }
+                    }
+                });
+
+                ((VH_IMAGE_ITEM) holder).mIv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mListener != null) {
+                            mListener.goToImageDetail(bean);
+                        }
+                    }
+                });
+
+            } else if (holder instanceof VH_DELETE_ITEM) {
+
+                if (mList.size() >= MAX_PIC_COUNT) {
+                    holder.itemView.setVisibility(View.GONE);
+                } else {
+                    holder.itemView.setVisibility(View.VISIBLE);
+                }
+
+                ((VH_DELETE_ITEM) holder).mDeleteIv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mListener != null) {
+                            mListener.addImage();
+                        }
+                    }
+                });
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mList.size();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return mList.get(position).getType();
+        }
+
+    }
+
+    private class VH_IMAGE_ITEM extends RecyclerView.ViewHolder {
+
+        private RoundImageView mIv;
+        private ImageView mDeleteIv;
+
+        public VH_IMAGE_ITEM(View itemView) {
+            super(itemView);
+            mDeleteIv = itemView.findViewById(R.id.deleteImageView);
+            mIv = itemView.findViewById(R.id.screenShotImageView);
+        }
+    }
+
+    private class VH_DELETE_ITEM extends RecyclerView.ViewHolder {
+
+        private ImageView mDeleteIv;
+
+        public VH_DELETE_ITEM(View itemView) {
+            super(itemView);
+            mDeleteIv = itemView.findViewById(R.id.screenShotImageView);
+        }
+    }
+
+
+    private class ImageBean {
+
+        private int type;
+        private String url;
+
+        public static final int TYPE_ADD = 0x01;
+        public static final int TYPE_IMAGE = 0x02;
+
+        public ImageBean() {
+
+        }
+
+        public ImageBean(int type, String url) {
+            this.type = type;
+            this.url = url;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+    public OnOperationListener mListener = new OnOperationListener() {
+        @Override
+        public void addImage() {
+            loadImages();
+        }
+
+        @Override
+        public void deleteImage(int position) {
+            mImageList.remove(position);
+            mAdapter.setList(mImageList);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void goToImageDetail(ImageBean bean) {
+            //  ImageOrderActivity.goToActivity(AddImageActivity.this, bean.getUrl());
+        }
+    };
+
+    private int getScreenShotWidth() {
+        int screenWidth = DimenUtils.getScreenWidth(this);
+        screenWidth = screenWidth - (int) (screenWidth * 1.0 / 9); // 除掉边框
+        return screenWidth / 4;
+    }
+
+    private static final String PATH = Environment
+            .getExternalStorageDirectory() + "/DCIM";
+
+    protected void loadImages() {
+
+        final CharSequence[] items = getResources().getStringArray(R.array.user_select_image_items);
+        AlertDialog dlg = new AlertDialog.Builder(this)
+                .setTitle("选取照片")
+                .setNegativeButton("取消", null)
+                .setItems(items, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        try {
+                            if (!SDCardUtils.exist()) {
+                                Utils.showShortToast(RecordAddActivity.this, "SD卡被占用或不存在");
+                            } else {
+                                if (item == 0) {
+                                    Uri imageURI = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                                    if (imageURI != null) {
+                                        try {
+                                            if (ContextCompat.checkSelfPermission(RecordAddActivity.this,
+                                                    Manifest.permission.READ_EXTERNAL_STORAGE)
+                                                    != PackageManager.PERMISSION_GRANTED) {
+                                                ActivityCompat.requestPermissions(RecordAddActivity.this,
+                                                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+                                            } else {
+                                                Intent intent = new Intent(Intent.ACTION_PICK, imageURI);
+                                                startActivityForResult(intent, FEEDBACK_LOAD_IMAGES_RESULT);
+                                            }
+                                        } catch (Exception e) {
+                                            Log.w("FeedbackPresenter", "loadImages: " + e.getMessage());
+                                        }
+                                    }
+                                } else {
+                                    if (ContextCompat.checkSelfPermission(RecordAddActivity.this,
+                                            Manifest.permission.READ_EXTERNAL_STORAGE)
+                                            != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(RecordAddActivity.this,
+                                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                            != PackageManager.PERMISSION_GRANTED) {
+                                        ActivityCompat.requestPermissions(RecordAddActivity.this,
+                                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                                    } else {
+                                        //  Intent intent = new Intent(Intent.ACTION_PICK, imageURI);
+                                        //   startActivityForResult(intent, FEEDBACK_LOAD_IMAGES_RESULT);
+                                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// 调用系统相机
+                                        name = android.text.format.DateFormat.format("yyyyMMdd_hhmmss",
+                                                Calendar.getInstance(Locale.CHINA))
+                                                + ".jpg";//以当前时间作为文件名
+                                        Uri imageUri = Uri.fromFile(new File(PATH, name));
+
+                                        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+                                        startActivityForResult(intent, REQUEST_CODE_CLOCK_TAKE_PHOTO);
+                                    }
+                                }
+                            }
+                        } catch (Exception e) {
+                            Utils.showShortToast(RecordAddActivity.this, "SD卡被占用或不存在");
+                        }
+                    }
+                }).create();
+        dlg.show();
+
+    }
+
+    String name;
+
+    public interface OnOperationListener {
+
+        void addImage();
+
+
+        void deleteImage(int position);
+
+
+        void goToImageDetail(ImageBean bean);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FEEDBACK_LOAD_IMAGES_RESULT) {
+            if (resultCode == RESULT_OK && data != null) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = null;
+                try {
+                    cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    if (cursor != null) {
+                        cursor.moveToFirst();
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        final String imagePath = cursor.getString(columnIndex);
+                        //根据手机屏幕设置图片宽度
+                        Bitmap bitmap = UploadFile.createImageThumbnail(imagePath, getScreenShotWidth(), true);
+                        if (bitmap != null) {
+                            insertImg(bitmap, imagePath, true);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //KLog.error(KLog.KLogFeature.toulan,"load image failed, msg:"+e.getMessage());
+                } finally {
+                    if (cursor != null) {
+                        cursor.close();
+                    }
+                }
+            }
+        } else if (requestCode == REQUEST_CODE_CLOCK_TAKE_PHOTO) {
+            if (resultCode == RESULT_OK) {
+
+                try {
+                    String filePath = PATH + "/" + name;
+                    Bitmap bitmap = UploadFile.createImageThumbnail(filePath, getScreenShotWidth(), true);
+                    if (bitmap != null) {
+                        insertImg(bitmap, filePath, true);
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        }
+    }
+
+    public void insertImg(Bitmap bitmap, final String imagePath,
+                          boolean showDelImg) {
+
+        ImageBean bean = new ImageBean(ImageBean.TYPE_ADD, imagePath);
+        mImageList.add(0, bean);
+        mAdapter.notifyDataSetChanged();
+    }
+
 
     public void confirm() {
 
---
+++ app/src/main/java/com/bjxapp/worker/ui/view/activity/widget/MaxHeightRecyclerView.java
@@ -0,0 +1,34 @@
+package com.bjxapp.worker.ui.view.activity.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+import com.bjxapp.worker.ui.widget.DimenUtils;
+
+public class MaxHeightRecyclerView extends RecyclerView {
+
+    public MaxHeightRecyclerView(Context context) {
+        super(context);
+    }
+
+    public MaxHeightRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MaxHeightRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthSpec, int heightSpec) {
+
+        int maxHeight = DimenUtils.dp2px(170 ,getContext());
+
+
+        int heightSpecMod = MeasureSpec.makeMeasureSpec(maxHeight , MeasureSpec.AT_MOST);
+
+        super.onMeasure(widthSpec, heightSpecMod);
+    }
+}
