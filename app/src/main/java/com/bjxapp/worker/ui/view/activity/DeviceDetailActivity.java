deaf3a4147a2272d5f4f3558f9ab092b57f16fef
zhangdan <zhangdan@cmcm.com>
2019-02-24 02:26:42 +0800

    update layout .

---
+++ app/src/main/java/com/bjxapp/worker/ui/view/activity/DeviceDetailActivity.java
@@ -0,0 +1,15 @@
+package com.bjxapp.worker.ui.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class DeviceDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
